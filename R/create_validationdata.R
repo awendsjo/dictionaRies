024864e2005dfd65310fe9d43041f179fe4f7c1e
awendsjo <awendsjo@gmail.com>
2022-12-03 11:26:29 +0100

    Changes

---
+++ R/create_validationdata.R
@@ -0,0 +1,19 @@
+create_validationdata <- function(data,n){
+  val_data <- data.frame(data=sample(data,n))
+
+  val_data$populism <- 0
+  val_data$peoplecentrism <- 0
+  val_data$antielitism <- 0
+  val_data$peoplesovereignty <- 0
+
+  for (i in 1:n){
+    print(val_data[i,1])
+    val_data[i,2] <- readline(prompt="Populism?")
+    if (val_data[i,2]==1){
+      val_data[i,3] = readline(prompt="People centrism?")
+      val_data[i,3] = readline(prompt="Anti-elitism?")
+      val_data[i,3] = readline(prompt="People sovereignty?")
+    }
+  }
+  return(val_data)
+}
